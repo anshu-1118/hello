8518ff4f1d24efa51f43aff6a10b2b2230354285
anshu-1118 <anshugupta39096@gmail.com>
2025-11-21 01:41:18 +0530

    hello.c

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@ int main()
 {
     printf("Hello, World!\n");
     printf("This project uses the C17 language standard version.\n");
+    printf("C17, also known as ISO/IEC 9899:2018, is the latest standard for the C programming language, published in 2018.\n");
     return 0;
 }
